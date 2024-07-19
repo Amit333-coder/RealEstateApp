44d5343d496e51d7758a095bebd6321b57982bec
Amit333-coder <asuraj248@gmail.com>
2024-07-19 13:28:44 +0530

    responsive-navBar-created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
+
+import { useState } from "react"
 import "./navbar.scss"
 
 function Navbar(){
+    const [open,setOpen] = useState(false);
     return(
         <nav className="navbar">
             <div className="left">
                 <a className="logo" href="/">
                 <img src="/logo.png" alt="logo" />
-                <span>Real Estate</span>
+                <span className="cName">Real Estate</span>
                 </a>
 
-                <a href="/">Home</a>
+                <a href="/" >Home</a>
                 <a href="/">About</a>
                 <a href="/">Contact</a>
                 <a href="/">Agents</a>
@@ -17,6 +20,17 @@ function Navbar(){
             <div className="right">
                 <a href="/" className="Si">Sign in</a>
                 <a href="/" className="Su">Sign up</a>
+                <div className="menuIcon">
+                    <img src="/menu.png" alt="" onClick={()=>setOpen(prev => !prev)}/>
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/" >Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign in</a>
+                    <a href="/">Sign up</a>
+                </div>
             </div>
 
         </nav>
